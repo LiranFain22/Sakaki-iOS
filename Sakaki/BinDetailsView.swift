a4286a3c37752ed186a64c2c2b586e581766b1a7
Liran Fainshtein <lirandeveloper@gmail.com>
2023-06-26 17:12:06 +0300

    Added ImagePicker feature

--- Sakaki/BinDetailsView.swift
+++ Sakaki/BinDetailsView.swift
@@ -1,6 +1,8 @@
 import SwiftUI
+import FirebaseStorage
 import URLImage
 import FirebaseAuth
+import UIKit
 
 struct BinDetailsView: View {
     @State var bin: Bin
@@ -11,6 +13,11 @@ struct BinDetailsView: View {
     @Environment(\.presentationMode) var presentationMode
     
     @State private var isActionSheetPresented = false
+    @State private var isImageFullScreen = false
+    @State private var isShowingImagePicker = false
+    @State private var isUsingCamera = false
+    @State private var cameraButtonPressed = false
+    @State private var selectedImage: UIImage?
     
     var body: some View {
         VStack {
@@ -20,12 +27,23 @@ struct BinDetailsView: View {
                 .padding()
             
             VStack(spacing: 10) {
-                URLImage(URL(string: bin.imageURL)!) { image in
-                    image
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
+                Button {
+                    // Activate full screen image view
+                    isImageFullScreen = true
+                } label: {
+                    URLImage(URL(string: bin.imageURL)!) { image in
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    }
                 }
+                .fullScreenCover(isPresented: $isImageFullScreen) {
+                    // Full screen image view
+                    ImageView(url: bin.imageURL, isImageFullScreen: $isImageFullScreen)
+                }
+                
                 Divider()
+                
                 HStack {
                     Text("Status: ")
                         .bold()
@@ -41,34 +59,31 @@ struct BinDetailsView: View {
                 }
                 Divider()
                 VStack {
-                    Button {
-                        isActionSheetPresented = true
-                    } label: {
-                        Text("Update Station")
-                            .font(.title2)
-                            .padding()
-                        Image(systemName: "newspaper")
-                            .font(.title2)
-                            .padding()
-                    }
-                    .foregroundColor(.white)
-                    .background(.green)
-                    .cornerRadius(20)
-                    .onChange(of: bin.status) { newStatus in
-                        updateBinStatus(status: newStatus)
-                    }
-                    
-                    
-                    Divider()
                     
                     HStack {
+                        
+                        Spacer()
+                        
+                        Button {
+                            isActionSheetPresented = true
+                        } label: {
+                            Image(systemName: "newspaper")
+                                .font(.title2)
+                                .padding()
+                        }
+                        .foregroundColor(.white)
+                        .background(.green)
+                        .cornerRadius(20)
+                        .onChange(of: bin.status) { newStatus in
+                            updateBinStatus(status: newStatus)
+                        }
+                        
+                        Spacer()
+                        
                         Button {
                             isBinSelectedToRoute.toggle()
                             presentationMode.wrappedValue.dismiss()
                         } label: {
-                            Text("Route")
-                                .font(.title2)
-                                .padding()
                             Image(systemName: "arrow.triangle.turn.up.right.diamond")
                                 .font(.title2)
                                 .padding()
@@ -80,24 +95,43 @@ struct BinDetailsView: View {
                         Spacer()
                         
                         Button {
-                            presentationMode.wrappedValue.dismiss()
+                            cameraButtonPressed = true
                         } label: {
-                            Text("Return")
-                                .font(.title2)
-                                .padding()
-                            Image(systemName: "return")
+                            Image(systemName: "camera")
                                 .font(.title2)
                                 .padding()
                         }
                         .foregroundColor(.white)
-                        .background(.red)
+                        .background(.purple)
                         .cornerRadius(20)
+                        
+                        Spacer()
                     }
+                    
+                    Divider()
+                    
+                    Button {
+                        presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        Text("Return")
+                            .font(.title2)
+                            .padding()
+                        Image(systemName: "return")
+                            .font(.title2)
+                            .padding()
+                    }
+                    .foregroundColor(.white)
+                    .background(.gray)
+                    .cornerRadius(20)
+                    .padding()
                 }
             }
             .padding()
             
             Spacer()
+        }
+        .sheet(isPresented: $isShowingImagePicker, onDismiss: loadImage) {
+            ImagePicker(selectedImage: $selectedImage, isUsingCamera: $isUsingCamera)
         }
         .actionSheet(isPresented: $isActionSheetPresented) {
             ActionSheet(title: Text("Select Status"), buttons: [
@@ -116,10 +150,23 @@ struct BinDetailsView: View {
                 .cancel()
             ])
         }
+        .actionSheet(isPresented: $cameraButtonPressed) {
+            ActionSheet(title: Text("Select Status"), buttons: [
+                .default(Text("Camera")) {
+                    isUsingCamera = true
+                    isShowingImagePicker = true
+                },
+                .default(Text("From Gallery")) {
+                    isUsingCamera = false
+                    isShowingImagePicker = true
+                },
+                .cancel()
+            ])
+        }
     }
     
     private func updateBinStatus(status: String) {
-        dataManager.updateBin(bin: bin, status: status)
+        dataManager.updateBinStatus(bin: bin, status: status)
         
         // Update the local bin object with the new status and last update
         bin.status = status
@@ -137,6 +184,79 @@ struct BinDetailsView: View {
         } else {
             print("Error - Can't get user from FirebaseAuth (updateUserData function)")
             Helper.showAlert(title: "Error", message: "Something went wrong...")
+        }
+    }
+    
+    private func loadImage() {
+        guard let selectedImage = selectedImage else { return }
+        
+        // Convert the selected image to data
+        guard let imageData = selectedImage.jpegData(compressionQuality: 0.8) else {
+            print("Error converting image to data")
+            Helper.showAlert(title: "Error", message: "Something went wrong...")
+            return
+        }
+        
+        // Create a unique filename for the image using a timestamp
+        let filename = "\(UUID().uuidString).jpg"
+        
+        // Create a reference to the Firebase Storage bucket and the desired storage location
+        let storage = Storage.storage()
+        let storageRef = storage.reference().child("images").child(filename)
+        
+        // Create the metadata for the image
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpeg"
+        
+        // Upload the image data to Firebase Storage
+        let uploadTask = storageRef.putData(imageData, metadata: metadata) { metadata, error in
+            if let error = error {
+                print("Error uploading image to Firebase Storage: \(error.localizedDescription)")
+                Helper.showAlert(title: "Error", message: "Something went wrong...")
+            } else {
+                // Image uploaded successfully, retrieve the download URL of the uploaded image
+                storageRef.downloadURL { url, error in
+                    if let error = error {
+                        print("Error retrieving download URL: \(error.localizedDescription)")
+                        Helper.showAlert(title: "Error", message: "Something went wrong...")
+                    } else {
+                        if let downloadURL = url {
+                            // The download URL of the uploaded image
+                            let imageURL = downloadURL.absoluteString
+                            // Update bin with the imageURL
+                            dataManager.updateBinPhoto(bin: bin, imageURL: imageURL)
+                        }
+                    }
+                }
+            }
+        }
+        
+        // You can also observe the upload progress if needed
+        uploadTask.observe(.progress) { snapshot in
+            guard let progress = snapshot.progress else { return }
+            let percentComplete = Double(progress.completedUnitCount) / Double(progress.totalUnitCount) * 100.0
+            print("Upload progress: \(percentComplete)%")
+        }
+    }
+}
+
+struct ImageView: View {
+    let url: String
+    @Binding var isImageFullScreen: Bool
+    
+    var body: some View {
+        VStack {
+            URLImage(URL(string: url)!) { image in
+                image
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+            .ignoresSafeArea()
+            .onTapGesture {
+                // Close full screen image view
+                isImageFullScreen = false
+            }
         }
     }
 }
--- Sakaki/DataManager.swift
+++ Sakaki/DataManager.swift
@@ -73,7 +73,7 @@ class DataManager: ObservableObject {
         }
     }
     
-    func updateBin(bin: Bin, status: String) {
+    func updateBinStatus(bin: Bin, status: String) {
         let db = Firestore.firestore()
         let ref = db.collection("Bins").document(bin.id)
         ref.updateData([
@@ -86,6 +86,28 @@ class DataManager: ObservableObject {
                 // Update the bin's status in the local array
                 if let index = self.bins.firstIndex(where: { $0.id == bin.id }) {
                     self.bins[index].status = status
+                    self.bins[index].lastUpdate = Date.now
+                }
+                
+                // Publish the changes to trigger view update
+                self.objectWillChange.send()
+            }
+        }
+    }
+    
+    func updateBinPhoto(bin: Bin, imageURL: String) {
+        let db = Firestore.firestore()
+        let ref = db.collection("Bins").document(bin.id)
+        ref.updateData([
+            "imageURL": imageURL,
+            "lastUpdate": Timestamp(date: Date.now)
+        ]) { error in
+            if let error = error {
+                Helper.showAlert(title: "Error", message: error.localizedDescription)
+            } else {
+                // Update the bin's imageURL in the local array
+                if let index = self.bins.firstIndex(where: { $0.id == bin.id }) {
+                    self.bins[index].imageURL = imageURL
                     self.bins[index].lastUpdate = Date.now
                 }
                 
